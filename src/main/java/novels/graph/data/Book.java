0e1e60aff0040e34aa038ae27b892abc0045ee5b
derek <dereknelson01@gmail.com>
2017-12-08 16:27:19 -0500

    Push

--- src/main/java/novels/graph/data/Book.java
+++ src/main/java/novels/graph/data/Book.java
@@ -1,14 +1,20 @@
 package novels.graph.data;
 
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
+@NodeEntity
 public class Book {
 
     String title;
     String author;
 
     LocalDateTime published;
+
+    @Relationship
     List<Character> characters;
 
 }
